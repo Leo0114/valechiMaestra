7a556f5dbbefdd28b6d4153e1eea0d4038d7eee8
Leo0114 <leonel0614@gmail.com>
2023-05-14 00:45:04 -0600

    ghpages deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
 import compress from "astro-compress";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), compress()]
+  site: "https://Leo0114.github.io",
+  base: "/valechiMaestra",
+  integrations: [tailwind(), compress()],
 });
